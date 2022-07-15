8fa8ee20224ee2cda5ab1b4d7877a3d5bccf4818
jmedeiros11 <jmedeiros1011@gmail.com>
2022-07-14 21:01:58 -0400

    main updated and home page struct created

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text('Home Page')));
+
+    return Scaffold(
+        // add body
+
+       );
   }
 }
