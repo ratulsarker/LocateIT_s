193ecec1181bb8a31fe20b153323f27ab6e57903
ratulsarker <ratulsarker05@hotmail.com>
2022-07-16 01:38:10 -0400

    added gradient to login page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -10,76 +10,85 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 117, 246, 255),
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              const SizedBox(height: 25),
-              // Text
-              const Text(
-                'LocateIT Login',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 25,
-                ),
-              ),
-              const SizedBox(height: 25),
-
-              const Text(
-                'Welcome back, you\'ve been missed!',
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-
-              const SizedBox(height: 20),
-              //email text
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: const Color.fromARGB(255, 255, 255, 255),
-                    border: Border.all(
-                        color: const Color.fromARGB(255, 104, 97, 97)),
-                    borderRadius: BorderRadius.circular(12),
+    return Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+        begin: Alignment.centerLeft,
+        end: Alignment.centerRight,
+        colors: [Colors.purple, Colors.blue],
+      )),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              children: [
+                const SizedBox(height: 185),
+                // Text
+                const Text(
+                  'LocateIT Login',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
                   ),
-                  child: const TextField(
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: 'Email',
+                ),
+                const SizedBox(height: 25),
+
+                const Text(
+                  'Welcome back, you\'ve been missed!',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+
+                const SizedBox(height: 20),
+                //email text
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 255, 255, 255),
+                      border: Border.all(
+                          color: const Color.fromARGB(255, 104, 97, 97)),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: const TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Email',
+                        prefixIcon: Icon(Icons.email_outlined),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 10),
-              //password text
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: const Color.fromARGB(255, 255, 255, 255),
-                    border: Border.all(
-                        color: const Color.fromARGB(255, 104, 97, 97)),
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  child: const TextField(
-                    decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'password',
-                        prefixIcon: Icon(Icons.security_outlined)),
+                const SizedBox(height: 10),
+                //password text
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 255, 255, 255),
+                      border: Border.all(
+                          color: const Color.fromARGB(255, 104, 97, 97)),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: const TextField(
+                      decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'password',
+                          prefixIcon: Icon(Icons.security_outlined)),
+                    ),
                   ),
                 ),
-              ),
-              //sign in button
+                //sign in button
 
-              //not a memeber? register now
-            ],
+                //not a memeber? register now
+              ],
+            ),
           ),
         ),
+        // ema
       ),
-      // ema
     );
   }
 }
